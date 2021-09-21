cf835d0feaf80f2ec5fd539d00e5f74f540d9231
Luiz Chaves <lucachaves@gmail.com>
2021-09-20 22:15:52 -0300

    gh-pages

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
 });
-module.exports = withNextra();
+module.exports = {
+  ...withNextra(),
+  env: {
+    PUBLIC_URL: 'https://ifpb.github.io/ls',
+    assetPrefix: './',
+  },
+  images: {
+    loader: 'imgix',
+    path: '',
+  },
+};
